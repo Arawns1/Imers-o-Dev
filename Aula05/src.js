36e93ff073549ccaaf0c1f03d5c93d7018746009
Gabriel <gabrieldamico22@gmail.com>
2022-09-13 20:31:13 -0300

    aula 05 parcialmente concluida

---
+++ Aula05/src.js
@@ -0,0 +1,23 @@
+
+var i = 0;
+
+function adicionarFilme() {
+  var input = document.getElementById("filme").value;
+  if(input.endsWith(".jpg") || input.endsWith(".png")){
+    listarFilmesNaTela(input)
+  }
+  else{
+    alert("Formato de inválido imagem!")
+    console.error("Formato de inválido imagem!")
+  }
+ 
+}
+
+function listarFilmesNaTela(input){
+  i++
+  var elementoFilmeFavorito = "<img src=" + input + ">";
+  var id = "<p>" + i + "</p>"
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML += elementoFilmeFavorito + id;
+  document.getElementById("filme").value = ' ';
+}
